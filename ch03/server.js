9d030c38c409c47b1f69dedfd9e85553025f87da
EthanK28 <kes0228@gmail.com>
2015-10-14 15:29:42 +0900

    ch03 p60

---
+++ ch03/server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+
+app.use('/', function(req, res) {
+    res.send('Hello World');
+});
+
+app.listen(3000);
+console.log('Serer running at http://localhost:3000');
+
+module.exports = app;
