bec398ca73c840214b7b8e26d0b9c844d921e0ee
EthanK28 <kes0228@gmail.com>
2015-10-14 21:12:53 +0900

    Static Files Handling

--- ch03/server.js
+++ ch03/server.js
@@ -1,6 +1,26 @@
 var express = require('express');
 var app = express();
 
+var hasName = function(req, res, next) {
+    if (req.param('name')) {
+        next();
+    } else {
+        res.send('What is your name?');
+    }
+}
+
+var hasSex = function(req, res, next) {
+    if (req.param('sex')) {
+        next();
+    } else {
+        res.send('What is your sex?');
+    }
+}
+
+var sayHello = function(req, res, next) {
+    
+}
+
 app.use('/', function(req, res) {
     res.send('Hello World');
 });
